e58018f11edb18f586317a3d99069444a846a43d
fabian <fbngrm@gmail.com>
2024-10-03 11:43:04 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # usb-dev
-Polybar usb drive module
+Polybar script for displaying and mounting/unmounting removable disks.
+
+- Left-click a partition to mount it
+- Left-click a mounted partition to open a terminal at the device's mount point
+- Middle-click a mounted partition to open a file explorer at the device's mount point
+- Right-click to unmount
+- Right-click unmounted device to power-off
+
+Supports drives with multiple partitions and each partition can be mounted individually. Will display partition's label if present, otherwise the name of the partition itself.
+
+Honors theme colors in ~/.Xresources; Unmounted partitions will show as "red" and mounted as "green" (the resolved colors will be theme-dependent).
+
+Based on [polybar-removable-disks](https://github.com/howey-aus/polybar-removable-disks/tree/master)
+
+![polybar-removable-disks](screenshot.png)
+
+```
+[module/removable-disks]
+type = custom/script
+exec = ~/.config/polybar/scripts/removable_disks.sh
+```
+
+## Dependencies
+
+* `jq`
+* `udisks2`
+* `xrdb`
---
+++ usb-dev.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+LSBLK_COLS="NAME,RM,TYPE,MOUNTPOINT,LABEL"
+# UNMOUNTED_COLOR=$(xrdb -query | grep "*color9" | cut -f 2)
+# MOUNTED_COLOR=$(xrdb -query | grep "*color10" | cut -f 2)
+
+# Define your preferred terminal and file explorer applications
+TERMINAL="kitty --directory"
+FILE_EXPLORER="thunar"
+
+# Create the JQ filter with no extra whitespaces or spaces between `%{A}` blocks
+JQ_SCR='.blockdevices[] |
+    select(.rm != false and .type == "part") |
+        if .mountpoint != null then
+            "%{F'$MOUNTED_COLOR'}"
+            + "%{A1:'$TERMINAL' " + .mountpoint + ":}"
+            + "%{A2:'$FILE_EXPLORER' " + .mountpoint + ":}"
+            + "%{A3:udisksctl unmount --no-user-interaction -b " + .name + ":}"
+        else
+            "%{F'$UNMOUNTED_COLOR'}"
+            + "%{A1:udisksctl mount --no-user-interaction -b " + .name + ":}"
+            + "%{A3:udisksctl power-off --no-user-interaction -b " + .name + ":}"
+        end
+        + " " + (.label // (.name | sub("/dev/" ; ""))) + "%{A}%{A}%{A}  "'
+
+# Execute lsblk and jq, and strip any excess newlines/spaces using tr and sed
+lsblk -pJlo $LSBLK_COLS |
+jq -r "$JQ_SCR" |
+tr '\n' ' ' |
+sed 's/[[:space:]]\{2,\}/ /g' | # Replace multiple spaces with a single space
+sed 's/[[:space:]]*$//' # Remove trailing spaces
